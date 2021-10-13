a00b303c6da83d4aa340906fda5af51c700c6556
cominixo <22600756+cominixo@users.noreply.github.com>
2021-10-13 20:28:03 -0300

    Batch draw calls for performance

--- src/main/java/me/cominixo/betterf3/mixin/DebugMixin.java
+++ src/main/java/me/cominixo/betterf3/mixin/DebugMixin.java
@@ -1,17 +1,22 @@
 package me.cominixo.betterf3.mixin;
 
 import com.google.common.base.Strings;
+import com.mojang.blaze3d.systems.RenderSystem;
 import me.cominixo.betterf3.config.GeneralOptions;
 import me.cominixo.betterf3.modules.BaseModule;
 import me.cominixo.betterf3.modules.MiscLeftModule;
 import me.cominixo.betterf3.modules.MiscRightModule;
+import me.cominixo.betterf3.utils.PositionEnum;
+import me.cominixo.betterf3.utils.Utils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.DebugHud;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Util;
+import net.minecraft.util.math.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -30,11 +35,12 @@ import static net.minecraft.client.gui.DrawableHelper.fill;
 public abstract class DebugMixin {
 
     @Shadow @Final private MinecraftClient client;
-    @Shadow @Final private TextRenderer fontRenderer;
 
     @Shadow protected abstract List<String> getLeftText();
 
     @Shadow protected abstract List<String> getRightText();
+
+    @Shadow @Final private TextRenderer textRenderer;
 
 
     public List<Text> getNewLeftText() {
@@ -94,30 +100,103 @@ public abstract class DebugMixin {
 
         List<Text> list = getNewRightText();
 
+        VertexConsumerProvider.Immediate immediate = getImmediate(PositionEnum.RIGHT, list, matrixStack);
+
         for (int i = 0; i < list.size(); i++) {
 
             if (!Strings.isNullOrEmpty(list.get(i).getString())) {
                 int height = 9;
-                int width = this.fontRenderer.getWidth(list.get(i).getString());
+                int width = this.textRenderer.getWidth(list.get(i).getString());
                 int windowWidth = (int)(this.client.getWindow().getScaledWidth() / GeneralOptions.fontScale) - 2 - width;
                 if (GeneralOptions.enableAnimations) {
                     windowWidth += xPos;
                 }
                 int y = 2 + height * i;
 
-                fill(matrixStack, windowWidth - 1, y - 1, windowWidth + width + 1, y + height - 1, GeneralOptions.backgroundColor);
+                this.textRenderer.draw(list.get(i), windowWidth, y, 0xE0E0E0, GeneralOptions.shadowText, matrixStack.peek().getModel(), immediate, false, 0, 15728880);
 
-                if (GeneralOptions.shadowText) {
-                    this.fontRenderer.drawWithShadow(matrixStack, list.get(i), windowWidth, (float)y, 0xE0E0E0);
-                } else {
-                    this.fontRenderer.draw(matrixStack, list.get(i), windowWidth, (float)y, 0xE0E0E0);
-                }
 
 
             }
         }
 
+        immediate.draw();
+
         ci.cancel();
+
+    }
+
+    public VertexConsumerProvider.Immediate getImmediate(PositionEnum pos, List<Text> list, MatrixStack matrixStack) {
+
+
+        float f = (float)(GeneralOptions.backgroundColor >> 24 & 255) / 255.0F;
+        float g = (float)(GeneralOptions.backgroundColor >> 16 & 255) / 255.0F;
+        float h = (float)(GeneralOptions.backgroundColor >> 8 & 255) / 255.0F;
+        float k = (float)(GeneralOptions.backgroundColor & 255) / 255.0F;
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        RenderSystem.enableBlend();
+        RenderSystem.disableTexture();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+
+        for (int i = 0; i < list.size(); i++) {
+            int height = 9;
+            int width = this.textRenderer.getWidth(list.get(i).getString());
+            int y = 2 + height * i;
+
+            int x1, x2, y1, y2, j;
+
+            if (pos == PositionEnum.RIGHT) {
+                int windowWidth = (int) (this.client.getWindow().getScaledWidth() / GeneralOptions.fontScale) - 2 - width;
+                if (GeneralOptions.enableAnimations) {
+                    windowWidth += xPos;
+                }
+
+                x1 = windowWidth - 1;
+                x2 = windowWidth + width + 1;
+                y1 = y - 1;
+                y2 = y + height - 1;
+            } else {
+                int xPosLeft = 2;
+
+                if (GeneralOptions.enableAnimations) {
+                    xPosLeft -= xPos;
+                }
+                x1 = 1 + xPosLeft;
+                x2 = width + 3 + xPosLeft;
+                y1 = y - 1;
+                y2 = y + height - 1;
+            }
+
+            Matrix4f matrix = matrixStack.peek().getModel();
+
+            if (x1 < x2) {
+                j = x1;
+                x1 = x2;
+                x2 = j;
+            }
+
+            if (y1 < y2) {
+                j = y1;
+                y1 = y2;
+                y2 = j;
+            }
+
+
+            bufferBuilder.vertex(matrix, (float)x1, (float)y2, 0.0F).color(g, h, k, f).next();
+            bufferBuilder.vertex(matrix, (float)x2, (float)y2, 0.0F).color(g, h, k, f).next();
+            bufferBuilder.vertex(matrix, (float)x2, (float)y1, 0.0F).color(g, h, k, f).next();
+            bufferBuilder.vertex(matrix, (float)x1, (float)y1, 0.0F).color(g, h, k, f).next();
+
+        }
+        bufferBuilder.end();
+        BufferRenderer.draw(bufferBuilder);
+        RenderSystem.enableTexture();
+        RenderSystem.disableBlend();
+
+        return VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
+
 
     }
 
@@ -128,15 +207,14 @@ public abstract class DebugMixin {
         if (GeneralOptions.disableMod) {
             return;
         }
-
         List<Text> list = getNewLeftText();
+        VertexConsumerProvider.Immediate immediate = getImmediate(PositionEnum.LEFT, list, matrixStack);
 
         for (int i = 0; i < list.size(); i++) {
 
             if (!Strings.isNullOrEmpty(list.get(i).getString())) {
 
                 int height = 9;
-                int width = this.fontRenderer.getWidth(list.get(i).getString());
                 int y = 2 + height * i;
                 int xPosLeft = 2;
 
@@ -144,17 +222,12 @@ public abstract class DebugMixin {
                     xPosLeft -= xPos;
                 }
 
-                fill(matrixStack, 1 + xPosLeft, y - 1, width + 3 + xPosLeft, y + height - 1, GeneralOptions.backgroundColor);
-
-                if (GeneralOptions.shadowText) {
-                    this.fontRenderer.drawWithShadow(matrixStack, list.get(i), xPosLeft, (float)y, 0xE0E0E0);
-                } else {
-                    this.fontRenderer.draw(matrixStack, list.get(i), xPosLeft, (float) y, 0xE0E0E0);
-                }
-
+                this.textRenderer.draw(list.get(i), xPosLeft, y, 0xE0E0E0, GeneralOptions.shadowText, matrixStack.peek().getModel(), immediate, false, 0, 15728880);
 
             }
         }
+
+        immediate.draw();
 
         ci.cancel();
 
