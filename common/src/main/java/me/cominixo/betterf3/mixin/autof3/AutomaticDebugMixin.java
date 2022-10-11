b84d77a6160a08a542d102c2531d5302d68801d7
TreyRuffy <treyruffy@users.noreply.github.com>
2022-10-11 01:41:34 -0600

    fix: autostart F3 menu

--- common/src/main/java/me/cominixo/betterf3/mixin/autof3/AutomaticDebugMixin.java
+++ common/src/main/java/me/cominixo/betterf3/mixin/autof3/AutomaticDebugMixin.java
@@ -1,7 +1,7 @@
 package me.cominixo.betterf3.mixin.autof3;
 
 import me.cominixo.betterf3.config.GeneralOptions;
-import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.GameOptions;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,10 +12,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  * Mixin to automatically start the F3 menu.
  */
 
-@Mixin(InGameHud.class)
+@Mixin(MinecraftClient.class)
 public class AutomaticDebugMixin {
 
-  @Redirect(method = "clear", at = @At(value = "FIELD", target = "Lnet/minecraft/client/option/GameOptions;debugEnabled:Z", opcode = Opcodes.PUTFIELD))
+  @Redirect(method = "openScreen", at = @At(value = "FIELD", target = "Lnet/minecraft/client/option/GameOptions;debugEnabled:Z", opcode = Opcodes.PUTFIELD))
   private void removeDebugCrosshair(final GameOptions instance, final boolean value) {
     if (!GeneralOptions.disableMod) {
       if (GeneralOptions.autoF3) {
